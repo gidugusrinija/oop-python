192c6c680420cf31543995d9a8971578f1bc87e5
srinijagidugu <srinija.gidugu@exponentialai.com>
2026-03-14 17:21:55 +0530

    singleton class

---
+++ classes/singleton.py
@@ -0,0 +1,17 @@
+class A(object):
+    instance = None
+
+    def __new__(cls):
+        print("Entered __new__")
+        if not cls.instance:
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def __eq__(self, other):
+        return id(self) == id(other)
+
+
+a1 = A()
+b1 = A()
+
+print(a1.__eq__(b1))
