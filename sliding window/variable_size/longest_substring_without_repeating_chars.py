2ccf190ebb4082f00271c14568a6f00d2c70aa11
srinijagidugu <srinija.gidugu@exponentialai.com>
2026-03-22 22:54:06 +0530

    longest substring

---
+++ sliding window/variable_size/longest_substring_without_repeating_chars.py
@@ -0,0 +1,30 @@
+"""
+Longest Substring Without Repeating Characters
+Input: String s.
+
+Goal: Find the length of the longest substring without repeating characters.
+
+Example: s = "abcabcbb" → Output: 3 ("abc").
+"""
+
+
+def longest_substring(s):
+    left = 0
+    max_len = 0
+    start, end = -1, -1
+    char_map = {}
+
+    for right, val in enumerate(s):
+        if val in char_map and right > left:
+            left = char_map[val] + 1
+        char_map[val] = right
+        if right - left + 1 > max_len:
+            max_len = right - left + 1
+            start = left
+            end = right + 1
+    return max_len, start, end
+
+
+s = "abcdefghaxyzwstuvop"
+max_l, st, e = longest_substring(s)
+print(max_l, s[st:e])
