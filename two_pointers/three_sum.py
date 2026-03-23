1dd5a2243c5d1be6b27b7ba1c595c1ff8deb1850
srinijagidugu <srinija.gidugu@exponentialai.com>
2026-03-23 17:15:14 +0530

    three sum

---
+++ two_pointers/three_sum.py
@@ -0,0 +1,29 @@
+l = [1, 0, -2, -6, 5, 4, 3, 9, 4]
+t = 3
+
+def three_sum(l, t):
+    l = sorted(l)
+    i = 0
+    n = len(l)
+    res = set()
+
+    while i < n-2:
+        j = i + 1
+        k = n - 1
+        reqd_sum = t - l[i]
+        while j < k:
+            curr_sum = l[j] + l[k]
+
+            if reqd_sum == curr_sum:
+                res.add((l[i], l[j], l[k]))
+                j += 1
+                k -= 1
+            elif curr_sum > reqd_sum:
+                k -= 1
+            else:
+                j += 1
+        i += 1
+
+    return res
+
+print(three_sum(l, t))
